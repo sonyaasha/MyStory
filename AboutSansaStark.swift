9772e3df09018e0002cca8acfd0e416975d6259c
Sofia Toropova <85236921+sonyaasha@users.noreply.github.com>
2023-02-26 18:42:05 -0500

    Files Added

---
+++ AboutSansaStark.swift
@@ -0,0 +1,30 @@
+//
+//  AboutSansaStark.swift
+//  MyStory
+//
+//  Created by Sofia Toropova on 2/25/23.
+//
+
+import UIKit
+
+class AboutSansaStark: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Sofia Toropova on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var sansaButton: UIButton!
+    
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var beforeButton: UIButton!
+    @IBOutlet weak var duringButton: UIButton!
+    @IBOutlet weak var afterButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
